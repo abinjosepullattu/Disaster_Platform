100654f7b3d1c342468cdc6530edbd08da2666e1
Abin Jose <joseabin291@gmail.com>
2025-09-21 15:30:26 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 // example Express
 
 app.use(cors({
-  origin: ["https://disaster-platform-frontend.vercel.app"], // replace with your real Vercel URL
+  origin: "https://disaster-platform-frontend-abinjosepullattus-projects.vercel.app", // replace with your real Vercel URL
   credentials: true
 }));
 
